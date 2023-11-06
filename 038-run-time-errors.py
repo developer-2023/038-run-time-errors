10678b9206fa2d6068bde2eff5b5bab6628b0eab
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-06 18:17:47 +0100

    Update 038-run-time-errors.py

--- 038-run-time-errors.py
+++ 038-run-time-errors.py
@@ -1,2 +1,8 @@
-x = int(input("What's x? "))
-print(f"x is {x}")
+print("Start")
+try:
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")
+print("The end")
