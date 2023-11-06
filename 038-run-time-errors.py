27b48348c4450efd3d96513b4de18031b791dbd7
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-06 23:41:08 +0100

    Update 038-run-time-errors.py

--- 038-run-time-errors.py
+++ 038-run-time-errors.py
@@ -1,8 +1,30 @@
-print("Start")
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-else:
-    print(f"x is {x}")
-print("The end")
+# First version of input number loop
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        print(f"x is {x}")        
+        break
+
+# Second version
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
+
+# Third version shorter but less flexible (no else options)
+while True:
+    try:
+        x = int(input("What's x? "))
+        break
+    except ValueError:
+        print("x is not an integer")
+
+print(f"x is {x}")
